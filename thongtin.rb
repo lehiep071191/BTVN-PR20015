a7e0143c9eb6b4dd4c58e89b39950e09cd56e4b0
lehiep071191 <good.boy.0991@gmail.com>
2020-07-27 10:36:52 +0700

    thongtincuahiep

---
+++ thongtin.rb
@@ -0,0 +1,9 @@
+
+puts "sơ yếu lý lịch của tôi"
+puts "\n \n "
+puts "tên tôi là lê hiệp"
+puts "tôi sinh ngày 07 tháng 11 năm 1991" 
+puts "quê quán : bình minh - to - hn"
+puts "tôi giới tính nam thẳng 100%"
+puts "sđt 0355965021"
+puts "địa chỉ: bình minh , thanh oai, hà nội"
